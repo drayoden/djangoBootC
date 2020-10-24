fd8316e2f9a3b2a37da789be037c3da8709540bf
daryn <daryn@hex50.com>
2020-10-24 14:02:29 -0700

    remove SECRET_KEY

--- bootc/settings.py
+++ bootc/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j%$pd09o8n6r6voz2+t^+r*&u-64s68fx_vr4lm!s&l!)pabby'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
